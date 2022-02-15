2693b0100c882bbbc0e43ce7f166cf6d81e4b7a9
grantrussell <grant.ellis.russell@gmail.com>
2022-02-15 15:36:35 -0600

    added models to python backend

---
+++ app/models/orders.py
@@ -0,0 +1,34 @@
+from .db import db
+
+
+class OrderDetail(db.Model):
+    __tablename__ = 'order_details'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Numeric(6,2), nullable=False)
+    sales_tax = db.Column(db.Numeric(6,2), nullable=False)
+    total = db.Column(db.Numeric(6,2), nullable=False)
+    delivered = db.Column(db.Boolean, nullable=False, default=False)
+    created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    updated_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+
+    user = db.relationship("User", back_populates="orders")
+    product = db.relationship("Product", back_populates="orders")
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'product_id': self.product_id,
+            'quantity': self.quantity,
+            'price': self.price,
+            'sales_tax': self.sales_tax,
+            'total': self.total,
+            'delivered': self.delivered,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
---
+++ app/models/product.py
@@ -0,0 +1,36 @@
+from .db import db
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+    attributes = db.Column(db.String, nullable=False)
+    original_price = db.Column(db.Numeric(6,2), nullable=False)
+    discount = db.Column(db.Integer, nullable=False, default=0)
+    final_price = db.Column(db.Numeric(6,2), nullable=False)
+    image_url1 = db.Column(db.String(500), nullable=False)
+    image_url2 = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    updated_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+
+    category = db.relationship("Category", back_populates="products")
+    cart_items = db.relationship("Product", back_populates="product")
+    orders = db.relationship("OrderDetail", back_populates="product")
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'category_id': self.category_id,
+            'attributes': self.attributes,
+            'original_price': self.original_price,
+            'discount': self.discount,
+            'final_price': self.final_price,
+            'image_url1': self.image_url1,
+            'image_url2': self.image_url2
+        }
