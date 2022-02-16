5ec86f93dc79a0a8954baa061daa71b03f6de2a3
grantrussell <grant.ellis.russell@gmail.com>
2022-02-16 10:26:33 -0600

    addedproduct api route, and set up product redux store, tested fetch on front end and all is well

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -4,14 +4,11 @@ from app.models import Product
 product_routes = Blueprint("products", __name__)
 
 @product_routes.route("/")
-def get_products():
-    """
-    Returns all product listings.
-    """
+def get_all_products():
     products = Product.query.all()
     return {"products": [product.to_dict() for product in products]}
 
 @product_routes.route('/<int:id>')
-def product(id):
+def get_product_by_id(id):
     product = Product.query.get(id)
     return product.to_dict()
