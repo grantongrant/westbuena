4df37a7c97b2c56a31a76bcbc98744bba8bfcd2b
grantrussell <grant.ellis.russell@gmail.com>
2022-02-21 19:40:23 -0600

    added basic styling to product pages

--- react-app/src/components/Products/Category.js
+++ react-app/src/components/Products/Category.js
@@ -2,8 +2,7 @@ import { useParams, NavLink } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import { getProductsByCategory } from '../../store/product';
 import { useDispatch, useSelector } from 'react-redux';
-
-
+import "./Products.css";
 
 function CategoryPage() {
   
@@ -18,21 +17,26 @@ function CategoryPage() {
 
     const categoryComponents = products?.map((product) => {
         return (
-          <li key={product.id}>
-            <NavLink to={`/products/${product.id}`}><img src={product.image_url1} alt="product"/></NavLink>
-          </li>
+          <div className="product-container" key={product.id}>
+            <div className="product-image-container">
+              <div><NavLink to={`/products/${product.id}`}><img src={product.image_url1} alt="product"/></NavLink></div>
+            </div>
+            <div className="product-name-card">{product.name}</div>
+            <div className="shipping-and-price">
+              <div className="free-shipping-product">Free Shipping</div>
+              <div className="product-price-card">${product.final_price}</div>
+            </div>
+          </div>
         );
       });
 
     return (
-        <div>
-            <h1>Hello from {category}!</h1>
+        <div className="category-container">
+          <>
             {categoryComponents}
+          </>
         </div>
-        
-        
     )
-
 }
 
 export default CategoryPage;
