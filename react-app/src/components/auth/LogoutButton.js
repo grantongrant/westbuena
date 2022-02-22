dc801bfa5b4e917e8d0bda9fdf8e7b4f12929721
grantrussell <grant.ellis.russell@gmail.com>
2022-02-21 21:33:23 -0600

    styled dropdown menus on login and logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
+import '../NavBar/NavBar.css';
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
@@ -11,7 +12,7 @@ const LogoutButton = () => {
     .then (() => history.push("/"))
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return <button className="logout-button-nav" onClick={onLogout}>Logout</button>;
 };
 
 export default LogoutButton;
