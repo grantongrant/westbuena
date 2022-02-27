aca9b7a2996d7a2e846e1602823640ad21025c88
grantrussell <grant.ellis.russell@gmail.com>
2022-02-27 16:58:29 -0600

    double-checked favorites code on sales page.

--- react-app/src/components/Products/Sale.js
+++ react-app/src/components/Products/Sale.js
@@ -4,10 +4,13 @@ import { getSaleProducts } from '../../store/product';
 import { useDispatch, useSelector } from 'react-redux';
 import {FaHeart} from 'react-icons/fa';
 import "./Products.css";
+import { addToFavoritesList, removeFromFavoritesList } from '../../store/favorite';
+
 
 function SalePage() {
   
     const dispatch = useDispatch();
+    const user = useSelector((state) => state.session.user)
     const productObject = useSelector((state) => state.product)
     const products = Object.values(productObject)
 
@@ -15,11 +18,28 @@ function SalePage() {
       dispatch(getSaleProducts())
     }, [dispatch]);
 
+    const addToFavs = async (product) => {
+      if (!user) {
+        alert(`We agree. ${product.name} is a great product. Please sign in or create an account to save your favorites.`)
+        return;
+      } else {
+        await dispatch(addToFavoritesList(user.id, product.id))
+        await dispatch(getSaleProducts())
+      }
+    }
+
+    const removeFromFavs = async (productId) => {
+      await dispatch(removeFromFavoritesList(user.id, productId))
+      await dispatch(getSaleProducts())
+    }
+
     const categoryComponents = products?.map((product) => {
         return (
           <div className="product-container" key={product.id}>
             <div className="product-image-container">
-              <div className="favorite-icon-category-page"><FaHeart/></div> 
+              {product.favorites.includes(user?.id) ?
+              <div className="favorited-icon-category-page" onClick={() => {removeFromFavs(product.id)}}><FaHeart/></div> :
+              <div className="favorite-icon-category-page" onClick={() => {addToFavs(product)}}><FaHeart/></div>} 
               <div><NavLink to={`/products/${product.id}`}>
                 <img className="image-1" src={product.image_url1} alt="product"/>
                   <img className="image-2" src={product.image_url2} alt="product"/>
